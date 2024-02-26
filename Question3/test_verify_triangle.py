f585699d7058789528731ff2909af8ed5b59ae4b
Veenanjali <91068676+Veenanjali@users.noreply.github.com>
2024-02-26 14:32:10 +0530

    added print statements

--- Question3/test_verify_triangle.py
+++ Question3/test_verify_triangle.py
@@ -1,4 +1,4 @@
-#from verify_triangle import Verify
+from verify_triangle import Verify
 
 class Test_triangle:
     @classmethod
@@ -14,17 +14,21 @@ class Test_triangle:
 
 
     def test_1(self):
+        print("test1")
         self.vt.equilateral()
         assert True == self.vt.check_trinagle()
     def test_2(self):
+        print("test2")
         self.vt.driver.refresh()
         self.vt.isoceles()
         assert True == self.vt.check_trinagle()
     def test_3(self):
+        print("test3")
         self.vt.driver.refresh()
         self.vt.scalene()
         assert True == self.vt.check_trinagle()
     def test_4(self):
+        print("test4")
         self.vt.driver.refresh()
         self.vt.notriangle()
         assert True == self.vt.check_trinagle()
